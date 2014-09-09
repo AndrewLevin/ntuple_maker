6a6dc4a29270bede5b8931d6f4d9ead4fcdccb8d
Andrew <anlevin@cern.ch>
2014-09-09 13:01:55 +0200

    no commit message

---
+++ ntuple_maker/python/ConfFile_cfg.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.TFileService = cms.Service("TFileService", fileName = cms.string("output_tree.root") )
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
+
+#process.load('EgammaAnalysis/ElectronTools/egmGsfElectronIDs_cff')
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+#        '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU40bx25_POSTLS170_V5-v2/00000/00CAA728-D626-E411-9112-00215AD4D6E2.root'
+        '/store/user/anlevin/data/MINIAOD/wpwp_13_tev_qed_4_qcd_0_v3/Merged.root'
+    )
+)
+
+process.demo = cms.EDAnalyzer('ntuple_maker',
+  vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+  muons = cms.InputTag("slimmedMuons"),
+  electrons = cms.InputTag("slimmedElectrons"),
+  taus = cms.InputTag("slimmedTaus"),
+  photons = cms.InputTag("slimmedPhotons"),
+  jets = cms.InputTag("slimmedJets"),
+  fatjets = cms.InputTag("slimmedJetsAK8"),
+  mets = cms.InputTag("slimmedMETs"),  
+)
+
+
+process.p = cms.Path(process.demo)
