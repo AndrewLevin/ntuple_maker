e326d5b7ad01a873a1be464aa20cd0ca9b68f257
Andrew <anlevin@cern.ch>
2014-09-09 13:05:36 +0200

    no commit message

--- ntuple_maker/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/ntuple_maker/ntuple_maker/',1)[0])+'/cfipython/slc6_amd64_gcc481/ntuple_maker/ntuple_maker')
