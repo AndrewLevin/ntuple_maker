ed959bac7d981486d834b3284e6caa3ba707557c
Andrew <anlevin@cern.ch>
2015-01-29 17:56:15 +0100

    no commit message

--- ntuple_maker/test/crabConfig_MC.py
+++ ntuple_maker/test/crabConfig_MC.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'flattish_nuples_v3'
+config.General.requestName = 'flattish_nuples_v9'
 config.General.workArea = 'crab_projects'
 
 config.section_("JobType")
@@ -11,14 +11,17 @@ config.JobType.psetName = 'ntuple_maker/ntuple_maker/python/ConfFile_cfg.py'
 
 config.section_("Data")
 #config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU40bx25_POSTLS170_V7-v2/MINIAODSIM'
-config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU40bx25_POSTLS170_V5-v2/MINIAODSIM'
-config.Data.dbsUrl = 'global'
+#config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU40bx25_POSTLS170_V5-v2/MINIAODSIM'
+#config.Data.inputDataset= '/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/Phys14DR-PU20bx25_trkalmb_PHYS14_25_V1-v1/MINIAODSIM'
+#config.Data.inputDataset= '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU4bx50_PHYS14_25_V1-v1/MINIAODSIM'
+config.Data.inputDataset= '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
+config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 1000
 #config.Data.lumiMask = 'lumi_mask_JSON.txt'
 config.Data.publication = True
-config.Data.publishDbsUrl = 'phys03'
-config.Data.publishDataName = 'flattish_ntuples_v3'
+config.Data.publishDBS = 'phys03'
+config.Data.publishDataName = 'flattish_ntuples_v9'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_CH_CERN'
