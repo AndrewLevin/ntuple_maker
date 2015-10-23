42fcabe4c4280d74c0056beb5c0b96a7cee0827f
Andrew <anlevin@cern.ch>
2015-10-23 14:26:08 +0200

    divide make loose lepton config files into electron and muon

---
+++ ntuple_maker/python/ConfFile_make_loose_muon_trees_cfg.py
@@ -0,0 +1,48 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.TFileService = cms.Service("TFileService", fileName = cms.string("output_tree.root") )
+
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+
+#process.load('EgammaAnalysis/ElectronTools/egmGsfElectronIDs_cff')
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+
+'/store/data/Run2015D/DoubleEG/MINIAOD/PromptReco-v3/000/257/400/00000/587D2C79-4C65-E511-A018-02163E0142E9.root',
+'/store/data/Run2015D/DoubleEG/MINIAOD/PromptReco-v3/000/257/400/00000/64CFDD24-5065-E511-9BAB-02163E011B22.root',
+'/store/data/Run2015D/DoubleEG/MINIAOD/PromptReco-v3/000/257/400/00000/7A458316-5065-E511-8FFC-02163E01421A.root',
+'/store/data/Run2015D/DoubleEG/MINIAOD/PromptReco-v3/000/257/400/00000/84057F14-5065-E511-BE28-02163E01412F.root',
+'/store/data/Run2015D/DoubleEG/MINIAOD/PromptReco-v3/000/257/400/00000/94B0A799-5065-E511-969F-02163E0133BA.root',
+'/store/data/Run2015D/DoubleEG/MINIAOD/PromptReco-v3/000/257/400/00000/98063C1D-4D65-E511-95E1-02163E011A31.root'
+
+
+#'/store/data/Run2015D/DoubleEG/MINIAOD/PromptReco-v3/000/256/584/00000/D4DF072A-855D-E511-B91B-02163E01468C.root',
+#'/store/data/Run2015D/DoubleEG/MINIAOD/PromptReco-v3/000/256/587/00000/560F124D-925D-E511-BC70-02163E011D99.root',
+
+
+    )
+)
+
+process.demo = cms.EDAnalyzer('make_loose_lepton_trees',
+  vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+  muons = cms.InputTag("slimmedMuons"),
+  electrons = cms.InputTag("slimmedElectrons"),
+  taus = cms.InputTag("slimmedTaus"),
+  photons = cms.InputTag("slimmedPhotons"),
+  jets = cms.InputTag("slimmedJets"),
+  fatjets = cms.InputTag("slimmedJetsAK8"),
+  mets = cms.InputTag("slimmedMETs"),  
+  prunedgenparticles = cms.InputTag("prunedGenParticles"),  
+  packedgenparticles = cms.InputTag("packedGenParticles"),  
+  isMC = cms.untracked.bool(False),
+  lepton_flavor = cms.untracked.string("muon")
+)
+
+
+process.p = cms.Path(process.demo)
--- ntuple_maker/test/crabConfig_make_loose_muon_trees_data.py
+++ ntuple_maker/test/crabConfig_make_loose_muon_trees_data.py
@@ -2,12 +2,12 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'flattish_ntuples_v107'
+config.General.requestName = 'fr_muon_ntuples_v202'
 config.General.workArea = 'crab_projects'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'ntuple_maker/ntuple_maker/python/ConfFile_make_loose_lepton_trees_cfg.py'
+config.JobType.psetName = 'ntuple_maker/ntuple_maker/python/ConfFile_make_loose_muon_trees_cfg.py'
 
 config.section_("Data")
 #config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU40bx25_POSTLS170_V7-v2/MINIAODSIM'
@@ -18,14 +18,17 @@ config.section_("Data")
 #config.Data.inputDataset= '/QCD_HT_1000ToInf_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
 #config.Data.inputDataset= '/QCD_HT-500To1000_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
 #config.Data.inputDataset= '/DoubleMuon/Run2015C-PromptReco-v1/MINIAOD'
-config.Data.inputDataset='/DoubleEG/Run2015C-PromptReco-v1/MINIAOD'
+
+#config.Data.inputDataset='/DoubleEG/Run2015D-PromptReco-v3/MINIAOD'
+config.Data.inputDataset='/DoubleMuon/Run2015D-PromptReco-v4/MINIAOD'
+
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 100
 #config.Data.lumiMask = 'lumi_mask_JSON.txt'
 config.Data.publication = True
 config.Data.publishDBS = 'phys03'
-config.Data.publishDataName = 'flattish_ntuples_v107'
+config.Data.publishDataName = 'fr_muon_ntuples_v202'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_CH_CERN'
