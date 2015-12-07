d7e86f30383201b3663b27ecd0e17f78b9491e0d
Andrew <anlevin@cern.ch>
2015-12-07 17:25:28 +0100

    add a lumi mask

--- ntuple_maker/test/crabConfig_make_loose_electron_trees_data.py
+++ ntuple_maker/test/crabConfig_make_loose_electron_trees_data.py
@@ -2,25 +2,19 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'fr_electron_ntuples_v202'
+config.General.requestName = 'fr_electron_ntuples_v4002'
 config.General.workArea = 'crab_projects'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'ntuple_maker/ntuple_maker/python/ConfFile_make_loose_electron_trees_cfg.py'
+config.JobType.psetName = 'ntuple_maker/ntuple_maker/python/ConfFile_make_loose_electron_trees_data_cfg.py'
 
 config.section_("Data")
-#config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU40bx25_POSTLS170_V7-v2/MINIAODSIM'
-#config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU40bx25_POSTLS170_V5-v2/MINIAODSIM'
-#config.Data.inputDataset= '/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/Phys14DR-PU20bx25_trkalmb_PHYS14_25_V1-v1/MINIAODSIM'
-#config.Data.inputDataset= '/QCD_HT-100To250_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
-#config.Data.inputDataset= '/QCD_HT_250To500_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
-#config.Data.inputDataset= '/QCD_HT_1000ToInf_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
-#config.Data.inputDataset= '/QCD_HT-500To1000_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
-#config.Data.inputDataset= '/DoubleMuon/Run2015C-PromptReco-v1/MINIAOD'
 
-config.Data.inputDataset='/DoubleEG/Run2015D-PromptReco-v4/MINIAOD'
-#config.Data.inputDataset='/DoubleMuon/Run2015D-PromptReco-v4/MINIAOD'
+#config.Data.inputDataset='/DoubleEG/Run2015B-PromptReco-v1/MINIAOD'
+config.Data.inputDataset='/DoubleEG/Run2015C-PromptReco-v1/MINIAOD'
+#config.Data.inputDataset='/DoubleEG/Run2015D-PromptReco-v3/MINIAOD'
+#config.Data.inputDataset='/DoubleEG/Run2015D-PromptReco-v4/MINIAOD'
 
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
@@ -28,7 +22,9 @@ config.Data.unitsPerJob = 100
 #config.Data.lumiMask = 'lumi_mask_JSON.txt'
 config.Data.publication = True
 config.Data.publishDBS = 'phys03'
-config.Data.publishDataName = 'fr_electron_ntuples_v202'
+config.Data.outputDatasetTag = 'fr_electron_ntuples_v4000'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_CH_CERN'
+
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
--- ntuple_maker/test/crabConfig_make_loose_muon_trees_data.py
+++ ntuple_maker/test/crabConfig_make_loose_muon_trees_data.py
@@ -2,24 +2,18 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'fr_muon_ntuples_v202'
+config.General.requestName = 'fr_muon_ntuples_v11002'
 config.General.workArea = 'crab_projects'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'ntuple_maker/ntuple_maker/python/ConfFile_make_loose_muon_trees_cfg.py'
+config.JobType.psetName = 'ntuple_maker/ntuple_maker/python/ConfFile_make_loose_muon_trees_data_cfg.py'
 
 config.section_("Data")
-#config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU40bx25_POSTLS170_V7-v2/MINIAODSIM'
-#config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU40bx25_POSTLS170_V5-v2/MINIAODSIM'
-#config.Data.inputDataset= '/QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8/Phys14DR-PU20bx25_trkalmb_PHYS14_25_V1-v1/MINIAODSIM'
-#config.Data.inputDataset= '/QCD_HT-100To250_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
-#config.Data.inputDataset= '/QCD_HT_250To500_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
-#config.Data.inputDataset= '/QCD_HT_1000ToInf_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
-#config.Data.inputDataset= '/QCD_HT-500To1000_13TeV-madgraph/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
-#config.Data.inputDataset= '/DoubleMuon/Run2015C-PromptReco-v1/MINIAOD'
 
-#config.Data.inputDataset='/DoubleEG/Run2015D-PromptReco-v3/MINIAOD'
+#config.Data.inputDataset='/DoubleMuon/Run2015B-PromptReco-v1/MINIAOD'
+#config.Data.inputDataset='/DoubleMuon/Run2015C-PromptReco-v1/MINIAOD'
+#config.Data.inputDataset='/DoubleMuon/Run2015D-PromptReco-v3/MINIAOD'
 config.Data.inputDataset='/DoubleMuon/Run2015D-PromptReco-v4/MINIAOD'
 
 config.Data.inputDBS = 'global'
@@ -28,7 +22,11 @@ config.Data.unitsPerJob = 100
 #config.Data.lumiMask = 'lumi_mask_JSON.txt'
 config.Data.publication = True
 config.Data.publishDBS = 'phys03'
-config.Data.publishDataName = 'fr_muon_ntuples_v202'
+config.Data.outputDatasetTag = 'fr_muon_ntuples_v11000'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_CH_CERN'
+
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+
+config.Site.blacklist = ["T1_US_FNAL","T2_DE_DESY","T2_US_UCSD","T2_BE_UCL"]
