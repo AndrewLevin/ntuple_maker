0677642d6f7ace200c7f8e42be7d8633ff719720
Andrew <anlevin@cern.ch>
2017-01-07 23:36:43 +0100

    add the entire 2016 dataset

--- ntuple_maker/test/crabConfig_data.py
+++ ntuple_maker/test/crabConfig_data.py
@@ -2,22 +2,41 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'flattish_nuples_v1005'
+config.General.requestName = 'flattish_nuples_80X_v2906'
 config.General.workArea = 'crab_projects'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'ntuple_maker/ntuple_maker/python/ConfFile_data_cfg.py'
+config.JobType.psetName = 'src/ntuple_maker/ntuple_maker/python/ConfFile_data_cfg.py'
 
 config.section_("Data")
 
-#config.Data.inputDataset = '/DoubleEG/Run2015C_25ns-16Dec2015-v1/MINIAOD'
-#config.Data.inputDataset = '/DoubleMuon/Run2015C_25ns-16Dec2015-v1/MINIAOD'
-#config.Data.inputDataset = '/MuonEG/Run2015C_25ns-16Dec2015-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleMuon/Run2016B-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleMuon/Run2016B-23Sep2016-v3/MINIAOD'
+#config.Data.inputDataset = '/DoubleMuon/Run2016C-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleMuon/Run2016D-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleMuon/Run2016E-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleMuon/Run2016F-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleMuon/Run2016G-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleMuon/Run2016H-PromptReco-v2/MINIAOD'
 
-#config.Data.inputDataset = '/MuonEG/Run2015D-16Dec2015-v1/MINIAOD'
-#config.Data.inputDataset = '/DoubleMuon/Run2015D-16Dec2015-v1/MINIAOD'
-config.Data.inputDataset = '/DoubleEG/Run2015D-16Dec2015-v2/MINIAOD'
+#config.Data.inputDataset = '/DoubleEG/Run2016B-23Sep2016-v2/MINIAOD'
+#config.Data.inputDataset = '/DoubleEG/Run2016B-23Sep2016-v3/MINIAOD'
+#config.Data.inputDataset = '/DoubleEG/Run2016C-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleEG/Run2016D-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleEG/Run2016E-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleEG/Run2016F-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleEG/Run2016G-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/DoubleEG/Run2016H-PromptReco-v2/MINIAOD'
+
+#config.Data.inputDataset = '/MuonEG/Run2016B-23Sep2016-v2/MINIAOD'
+#config.Data.inputDataset = '/MuonEG/Run2016B-23Sep2016-v3/MINIAOD'
+#config.Data.inputDataset = '/MuonEG/Run2016C-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/MuonEG/Run2016D-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/MuonEG/Run2016E-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/MuonEG/Run2016F-23Sep2016-v1/MINIAOD'
+#config.Data.inputDataset = '/MuonEG/Run2016G-23Sep2016-v1/MINIAOD'
+config.Data.inputDataset = '/MuonEG/Run2016H-PromptReco-v2/MINIAOD'
 
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
@@ -25,9 +44,12 @@ config.Data.unitsPerJob = 100
 #config.Data.lumiMask = 'lumi_mask_JSON.txt'
 config.Data.publication = True
 config.Data.publishDBS = 'phys03'
-config.Data.outputDatasetTag = 'flattish_ntuples_v1000'
+config.Data.outputDatasetTag = 'flattish_ntuples_80X_v2900'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_CH_CERN'
 
-config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON.txt'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt'
+
+#config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+
