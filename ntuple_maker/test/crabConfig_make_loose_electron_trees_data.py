0328dc08ddd873da175d088312b5022d25282f19
Andrew <anlevin@cern.ch>
2017-01-12 04:31:46 +0100

    no commit message

--- ntuple_maker/test/crabConfig_make_loose_electron_trees_data.py
+++ ntuple_maker/test/crabConfig_make_loose_electron_trees_data.py
@@ -2,17 +2,16 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'fr_electron_ntuples_data_v401'
+config.General.requestName = 'fr_electron_ntuples_data_v600'
 config.General.workArea = 'crab_projects'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'ntuple_maker/ntuple_maker/python/ConfFile_make_loose_electron_trees_data_cfg.py'
+config.JobType.psetName = 'src/ntuple_maker/ntuple_maker/python/ConfFile_make_loose_electron_trees_data_cfg.py'
 
 config.section_("Data")
 
-config.Data.inputDataset='/DoubleEG/Run2015C_25ns-16Dec2015-v1/MINIAOD'
-#config.Data.inputDataset='/DoubleEG/Run2015D-16Dec2015-v2/MINIAOD'
+config.Data.inputDataset='/DoubleEG/Run2016B-23Sep2016-v3/MINIAOD'
 
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
@@ -20,9 +19,12 @@ config.Data.unitsPerJob = 100
 #config.Data.lumiMask = 'lumi_mask_JSON.txt'
 config.Data.publication = True
 config.Data.publishDBS = 'phys03'
-config.Data.outputDatasetTag = 'fr_electron_ntuples_data_v400'
+config.Data.outputDatasetTag = 'fr_electron_ntuples_data_v600'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_CH_CERN'
 
-config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON.txt'
+#config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt'
+
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collis\
+ions16_JSON.txt'
