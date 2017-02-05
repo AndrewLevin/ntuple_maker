8348d24a8ea11156095e132d195d1cd1d45695a0
Andrew <anlevin@cern.ch>
2017-02-05 10:39:30 +0100

    synchronize with Guillelmo

--- ntuple_maker/python/ConfFile_make_loose_muon_trees_data_cfg.py
+++ ntuple_maker/python/ConfFile_make_loose_muon_trees_data_cfg.py
@@ -19,15 +19,96 @@ process.source = cms.Source("PoolSource",
     )
 )
 
+from CondCore.DBCommon.CondDBSetup_cfi import *
+
+jecString = cms.string('sqlite:src/ntuple_maker/ntuple_maker/data/jec/Spring16_25nsV10All_DATA.db')
+
+#jecString = cms.string('sqlite:src/ntuple_maker/ntuple_maker/data/Spring16_23Sep2016AllV1_DATA.db')
+
+process.jec = cms.ESSource("PoolDBESSource",
+      DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(0)
+        ),
+      timetype = cms.string('runnumber'),
+      toGet = cms.VPSet(
+      cms.PSet(
+            record = cms.string('JetCorrectionsRecord'),
+#            tag    = cms.string('JetCorrectorParametersCollection_Spring16_23Sep2016AllV1_DATA_AK4PFchs'),
+            tag    = cms.string('JetCorrectorParametersCollection_Spring16_25nsV10All_DATA_AK4PFchs'),
+            label  = cms.untracked.string('AK4PFchs')
+            ),
+      ## here you add as many jet types as you need
+      ## note that the tag name is specific for the particular sqlite file 
+      ), 
+      connect = jecString
+     # uncomment above tag lines and this comment to use MC JEC
+)
+
+process.es_prefer_jec = cms.ESPrefer('PoolDBESSource','jec')
+
+#see here https://twiki.cern.ch/twiki/bin/view/CMS/JetResolution and https://github.com/cms-jet/JRDatabase/tree/master/SQLiteFiles and https://twiki.cern.ch/twiki/bin/view/CMS/JERCReference
+
+jerString = cms.string('sqlite:src/ntuple_maker/ntuple_maker/data/jer/Spring16_25nsV6_DATA.db')
+
+#this is needed for met recalculation
+process.jer = cms.ESSource("PoolDBESSource",
+        CondDBSetup,
+        toGet = cms.VPSet(
+            # Resolution
+            cms.PSet(
+                record = cms.string('JetResolutionRcd'),
+                tag    = cms.string('JR_Spring16_25nsV6_DATA_PtResolution_AK4PFchs'),
+                label  = cms.untracked.string('AK4PFchs_pt')
+                ),
+
+            cms.PSet(
+                 record = cms.string('JetResolutionRcd'),
+                 tag    = cms.string('JR_Spring16_25nsV6_DATA_PhiResolution_AK4PFchs'),
+                 label  = cms.untracked.string('AK4PFchs_phi')
+                 ),     
+
+            # Scale factors
+            cms.PSet(
+                record = cms.string('JetResolutionScaleFactorRcd'),
+                tag    = cms.string('JR_Spring16_25nsV6_DATA_SF_AK4PFchs'),
+                label  = cms.untracked.string('AK4PFchs')
+                ),
+            ),
+        connect = jerString
+        )
+
+process.es_prefer_jer = cms.ESPrefer('PoolDBESSource', 'jer')
+
+from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
+
+updateJetCollection(
+   process,
+   jetSource = cms.InputTag('slimmedJets'),
+   labelName = 'UpdatedJEC',
+   jetCorrections = ('AK4PFchs', cms.vstring('L1FastJet',  'L2Relative', 'L3Absolute', 'L2L3Residual'), 'None') 
+)
+
+process.jecSequence = cms.Sequence(process.patJetCorrFactorsUpdatedJEC * process.updatedPatJetsUpdatedJEC)
+
+
+from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+runMetCorAndUncFromMiniAOD(process,
+           isData=True,
+
+           )
+
 process.demo = cms.EDAnalyzer('make_loose_lepton_trees',
   vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
   muons = cms.InputTag("slimmedMuons"),
   electrons = cms.InputTag("slimmedElectrons"),
   taus = cms.InputTag("slimmedTaus"),
   photons = cms.InputTag("slimmedPhotons"),
-  jets = cms.InputTag("slimmedJets"),
+jets = cms.InputTag("updatedPatJetsUpdatedJEC"),
+#  jets = cms.InputTag("slimmedJets"),
   fatjets = cms.InputTag("slimmedJetsAK8"),
-  mets = cms.InputTag("slimmedMETs"),  
+#  mets = cms.InputTag("slimmedMETs"),  
+mets = cms.InputTag('slimmedMETs','','Demo'),
+
   prunedgenparticles = cms.InputTag("prunedGenParticles"),  
   packedgenparticles = cms.InputTag("packedGenParticles"),  
   isMC = cms.untracked.bool(False),
@@ -40,5 +121,5 @@ rhoHLTElectronSelection = cms.InputTag("fixedGridRhoFastjetCentralCalo"),
 
 )
 
+process.p = cms.Path(process.fullPatMetSequence*process.jecSequence* process.demo)
 
-process.p = cms.Path(process.demo)
