0a1ffdb26d3a79804e14def7df5cf0b61c4c07fe
Andrew <anlevin@cern.ch>
2017-03-14 22:10:59 +0100

    no commit message

--- ntuple_maker/python/ConfFile_data_cfg.py
+++ ntuple_maker/python/ConfFile_data_cfg.py
@@ -23,31 +23,9 @@ process.source = cms.Source("PoolSource",
 
     fileNames = cms.untracked.vstring(
 
-#'/store/data/Run2016D/DoubleMuon/MINIAOD/23Sep2016-v1/90000/1EEEE4BB-0B8C-E611-B87F-3417EBE480D1.root'
+#'/store/data/Run2016H/MuonEG/MINIAOD/PromptReco-v2/000/283/876/00000/AA7CCD56-C19C-E611-8F3B-02163E011872.root'
 
-#'/store/data/Run2016E/DoubleEG/MINIAOD/23Sep2016-v1/90000/1AF5373A-3C91-E611-AFC4-0CC47A4C8E8A.root'
-
-#'/store/data/Run2016E/DoubleEG/MINIAOD/23Sep2016-v1/80000/5E6F8078-BE88-E611-9D50-0CC47A4D764C.root'
-
-#'/store/data/Run2016B/DoubleEG/MINIAOD/23Sep2016-v3/110000/408DD8D7-3398-E611-BDC5-02163E014AD3.root'
-
-#'/store/data/Run2016B/DoubleEG/MINIAOD/23Sep2016-v3/120000/18E0EC8C-3598-E611-BF53-0CC47A78A3B4.root'
-
-#'/store/data/Run2016B/DoubleEG/MINIAOD/23Sep2016-v3/00000/84FFB190-E697-E611-AB0A-003048F5E840.root'
-
-#'/store/data/Run2016B/DoubleEG/MINIAOD/23Sep2016-v3/120000/18E0EC8C-3598-E611-BF53-0CC47A78A3B4.root'
-
-#'/store/data/Run2016B/SingleMuon/MINIAOD/23Sep2016-v3/60000/C81A53E1-8598-E611-9D91-F45214939730.root'
-
-#'/store/data/Run2016B/SingleElectron/MINIAOD/23Sep2016-v3/60000/F0FEEBFC-959A-E611-8E9B-0025905AA9CC.root'
-
-#'/store/data/Run2016B/DoubleEG/MINIAOD/23Sep2016-v3/110000/3E3AEF51-4898-E611-85CA-0025905A6084.root'
-
-#'/store/data/Run2016B/DoubleMuon/MINIAOD/23Sep2016-v3/00000/CC154516-589B-E611-A533-008CFA197E0C.root'
-
-#'/store/data/Run2016B/DoubleMuon/MINIAOD/23Sep2016-v3/00000/00269AA1-479B-E611-A359-0025905A6082.root'
-
-'/store/data/Run2016H/MuonEG/MINIAOD/PromptReco-v2/000/283/876/00000/AA7CCD56-C19C-E611-8F3B-02163E011872.root'
+'/store/data/Run2016G/DoubleMuon/MINIAOD/03Feb2017-v1/100000/00182C13-EEEA-E611-8897-001E675A6C2A.root'
 
 ),
 
@@ -55,19 +33,19 @@ process.source = cms.Source("PoolSource",
 
 )
 
-#process.load("Configuration.Geometry.GeometryIdeal_cff") 
-#process.load('Configuration.StandardSequences.Services_cff')
-#process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.Geometry.GeometryIdeal_cff") 
+process.load('Configuration.StandardSequences.Services_cff')
+process.load("Configuration.StandardSequences.MagneticField_cff")
 
 #mc https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions#Global_Tags_for_Run2_MC_Producti
-#process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 
-#process.GlobalTag.globaltag = '80X_dataRun2_2016SeptRepro_v3'
+process.GlobalTag.globaltag = '80X_dataRun2_2016SeptRepro_v7'
 
 from CondCore.DBCommon.CondDBSetup_cfi import *
 
 #
-jecString = cms.string('sqlite:src/ntuple_maker/ntuple_maker/data/jec/Spring16_25nsV10All_DATA.db')
+jecString = cms.string('sqlite:src/ntuple_maker/ntuple_maker/data/jec/Summer16_23Sep2016AllV4_DATA.db')
 
 #jecString = cms.string('sqlite:src/ntuple_maker/ntuple_maker/data/Spring16_23Sep2016AllV1_DATA.db')
 
@@ -80,7 +58,7 @@ process.jec = cms.ESSource("PoolDBESSource",
       cms.PSet(
             record = cms.string('JetCorrectionsRecord'),
 #            tag    = cms.string('JetCorrectorParametersCollection_Spring16_23Sep2016AllV1_DATA_AK4PFchs'),
-            tag    = cms.string('JetCorrectorParametersCollection_Spring16_25nsV10All_DATA_AK4PFchs'),
+            tag    = cms.string('JetCorrectorParametersCollection_Summer16_23Sep2016AllV4_DATA_AK4PFchs'),
             label  = cms.untracked.string('AK4PFchs')
             ),
       ## here you add as many jet types as you need
@@ -143,11 +121,20 @@ process.cleanedMu = cms.EDProducer("PATMuonCleanerBySegments",
    passthrough = cms.string("isGlobalMuon && numberOfMatches >= 2"),
    fractionOfSharedSegments = cms.double(0.499))
 
-from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
-runMetCorAndUncFromMiniAOD(process,
-           isData=True,
+#from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+#runMetCorAndUncFromMiniAOD(process,
+#           isData=True,
                            
-           )
+#           )
+
+process.load('EgammaAnalysis.ElectronTools.regressionApplication_cff')
+from EgammaAnalysis.ElectronTools.regressionWeights_cfi import regressionWeights
+process = regressionWeights(process)
+
+process.load('EgammaAnalysis.ElectronTools.calibratedElectronsRun2_cfi')
+
+from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+runMetCorAndUncFromMiniAOD(process, isData=True)
 
 process.demo = cms.EDAnalyzer('ntuple_maker',
 
@@ -160,7 +147,8 @@ process.demo = cms.EDAnalyzer('ntuple_maker',
 #  lheruninfo = cms.InputTag("source"),
 #  lheevent = cms.InputTag("source"),
   #muons = cms.InputTag("slimmedMuons"),
-  electrons = cms.InputTag("slimmedElectrons"),
+#  electrons = cms.InputTag("slimmedElectrons"),
+  electrons = cms.InputTag("calibratedPatElectrons"),
   taus = cms.InputTag("slimmedTaus"),
   photons = cms.InputTag("slimmedPhotons"),
 #  jets = cms.InputTag("slimmedJets"),
@@ -181,10 +169,11 @@ rhoHLTElectronSelection = cms.InputTag("fixedGridRhoFastjetCentralCalo"),
 
 jes = cms.untracked.string("nominal"),
 jer = cms.untracked.string("nominal"),
-lheleptoninfo = cms.untracked.bool(False)
+lheleptoninfo = cms.untracked.bool(False),
+trigger_results_process = cms.untracked.string("HLT")
 )
 
-process.p = cms.Path(process.fullPatMetSequence*process.jecSequence*  process.cleanedMu*process.demo)
 
+process.p = cms.Path( process.regressionApplication * process.calibratedPatElectrons * process.fullPatMetSequence  * process.jecSequence*  process.cleanedMu*process.demo)
 
 
